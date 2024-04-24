ba9c413e5dfd6cc9ae5c16edb41ee5ea532eedbd
toledod <165401366+toledod@users.noreply.github.com>
2024-04-24 10:13:00 -0500

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 #
 @app.route('/hello')
 def my_function():
-    return "Hello World!"
+    return "you a bitch!"
 
 @app.route('/display/<word1>/<word2>')
 def my_display(word1, word2):
