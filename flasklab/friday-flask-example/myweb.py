03b5fccbf52895aa62f56aa19024c7b1ab35bf5f
toledod <165401366+toledod@users.noreply.github.com>
2024-04-26 10:37:17 -0500

    Update myweb.py

--- flasklab/friday-flask-example/myweb.py
+++ flasklab/friday-flask-example/myweb.py
@@ -7,4 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def welcomeScreen():
   return render_template("myIndex.html")
+
+@app.route('/name)
+def getName();
+  randomNum = random.randint(0, 19)
+  nameList = ["Gelad", "Howard", "Jose", "Veronica", "Frank", "Deven", "Pooh Shiesty", "Willian", "Craig", "Josie",
+              "Melanie", "Mellisa", "Bianca", "Steven", "Angel's bitch ass", "Allen", "Sharon", "Ben", "Kevin", "Alex"]
+  nameChosen = nameList[randomNum]
+  return render_template("gotName.html", name = nameChosen)
   
