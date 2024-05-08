c603988ebb9e0ba92598c46a2c47fdf8f0b39272
toledod <165401366+toledod@users.noreply.github.com>
2024-05-08 10:59:18 -0500

    Update myWeb.py

--- csslab/myWeb.py
+++ csslab/myWeb.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
   message = "Welcome to Get A Fortune Cookie!"
-  return render_template("myhtml.html", someText = message)
+  fortunePlcae = "click the cookie!"
+  return render_template("myhtml.html", someText = message, fortune = fortunePlace)
+  
+@app.route('/fortune')
+def cookieClick():
+  possibleF = ["you getting mad bitches", "you gonna die tmr", "good luck...", "you thought you were gonna get something good?",
+               "she wants you fr", "whatever you were thinking about doing, dont", "don't do it lil bro", "tell zoey she a bitch for good luck"]
+  num = Math.floor(Math.random() * 8)
+  fortune1 = possibleF[num]
+  return render_template("myhtml.html", someText = message, fortune = fortune1)
 
 if __name__ == '__main__':
     my_port = 5000
