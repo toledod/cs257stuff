9542b08b61195046e5fcd412acb7e92557914723
toledod <165401366+toledod@users.noreply.github.com>
2024-05-08 10:24:44 -0500

    Create myjs.js

---
+++ csslab/static/myjs.js
@@ -0,0 +1,5 @@
+function changeColor() {
+  the_heading = document.getElementById("hello");
+  the_heading.style.color = "red";
+  console.log("I just changed the color to: " + the_heading.style.color)  
+}
