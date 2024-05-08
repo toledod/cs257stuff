adb2ad64e1eefc739c88bbc8c9ab15f46283b274
toledod <165401366+toledod@users.noreply.github.com>
2024-05-08 12:35:30 -0500

    Update myWeb.py

--- csslab/myWeb.py
+++ csslab/myWeb.py
@@ -14,10 +14,12 @@ def welcome():
 @app.route('/fortune')
 def cookieClick():
   possibleF = ["you getting mad bitches", "you gonna die tmr", "good luck...", "you thought you were gonna get something good?",
-               "she wants you fr", "whatever you were thinking about doing, dont", "don't do it lil bro", "tell zoey she a bitch for good luck"]
-  num = random.randint(0, 7)
-  possibleF2 = ["tell zoey she a bitch for good luck", "if you see zoey today, you failing your next test", "call zoey, if she answers: good luck for 7 days",
-                "if zoey goes to HCI tmr, everyone gets a cookie"]
+               "she wants you fr", "whatever you were thinking about doing, dont", "don't do it lil bro", "tell zoey she a bitch for good luck",
+              "sometimes wisdom comes in shutting up", "think of 3 things, you getting none of them", "buy a tequila bottle", "buy 2 tequila bottles",
+              "google cute kittens rn", "shouldn't you be studying", "tell a freind your favorite song", "idk what to tell you lil bro", "try harder",
+              "change your fit it's ass", "what the dog doin?"]
+  num = random.randint(0, 18)
+ 
   message = "Welcome to Get A Fortune Cookie!"
   fortune1 = possibleF[num]
   return render_template("myhtml.html", someText = message, fortune = fortune1)
