275331cf882936eb87d65330bd0a977663e6ac4c
toledod <165401366+toledod@users.noreply.github.com>
2024-05-08 10:26:34 -0500

    Update myWeb.py

--- csslab/myWeb.py
+++ csslab/myWeb.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+  message = "Welcome to Get A Fortune Cookie!"
+  return render_template("myhtml.html", someText = message)
